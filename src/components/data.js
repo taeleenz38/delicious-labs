542d061d43bad3c2b7f6aaec3c9c9cf9ea3bc1c1
Tae Lee <taekyu.lee3@gmail.com>
2022-10-20 15:14:05 +1100

    replaced third project

--- src/components/data.js
+++ src/components/data.js
@@ -14,8 +14,8 @@ export default [
   },
   {
     id: 3,
-    title: "ARTea",
-    img: "artea.png",
-    description: "A collection that brings together fans of the ubiquitous Boba Tea. Holders are able to collect & modify their dynamic NFTs with whatever flavour combos they can dream of."
+    title: "Journey",
+    img: "journey.jpeg",
+    description: "A chill vibes art project where holders purchase a train ticket & board a train, and along the way they visit a number of stops, receive souvenirs, see the sights & ultimately enjoy the journey. Holders of tickets will be able to board at each stop, and can sell/trade their tickets away to new holders. Establish a community of people who enjoy travelling, chill vibes & cute art - possibly artist networking as well."
   }
 ]
