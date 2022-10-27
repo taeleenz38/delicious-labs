71e2e9c87b35d9ca457289b7a81047a4cd88370b
Tae Lee <taekyu.lee3@gmail.com>
2022-10-27 15:08:32 +1100

    mobile WIP

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Navbar2 from './components/Navbar2'
 import Navbar3 from './components/Navbar3'
 import Navbar4 from './components/Navbar4'
+import Navbarmobile from './components/Navbarmobile'
 import Footer from './components/Footer'
 import data from './components/data'
 import Card from './components/Card'
@@ -33,46 +34,69 @@ export default function App() {
   })
 
   return (
-    <div className="outer-wrapper">
-      <div className="wrapper">
-        <div className="inline-page ">
-          <Navbar />
-          <div className="center">
-            <div className="marquee">
+    <div>
+      <div className="outer-wrapper-mobile">
+        <div className="vertical-page">
+          <Navbarmobile />
+          <div className="center-mobile">
+            <div className="marquee-mobile">
               <p>Delicious <img src="/images/vector.png" alt="logo" /> Labs</p>
             </div>
           </div>
-          <Footer />
         </div>
-        <div className="inline-page ">
-          <Navbar2 />
-          <div className="center">
-            <div className="card-list">
-              {cards}
-            </div>
-          </div>
-        </div>
+        <div className="vertical-page">
 
-        <div className="inline-page ">
-          <Navbar3 />
-          <div className="center">
-            <div className="grid-wrapper">
-              {teamcards}
-            </div>
-          </div>
         </div>
+        <div className="vertical-page">
 
-        <div className="inline-page ">
-          <Navbar4 />
-          <div className="center">
-            <div className="aboutus">
-              <p>We are a small collective based internationally but focusing on building small scale NFT projects. We love to think outside the blockchain and create experiences that holders want to be apart of.</p>
-              <br></br>
-              <p>Exciting things coming soon...</p>
+        </div>
+        <div className="vertical-page">
+
+        </div>
+      </div>
+
+
+      <div className="outer-wrapper">
+        <div className="wrapper">
+          <div className="inline-page ">
+            <Navbar />
+            <div className="center">
+              <div className="marquee">
+                <p>Delicious <img src="/images/vector.png" alt="logo" /> Labs</p>
+              </div>
+            </div>
+            <Footer />
+          </div>
+          <div className="inline-page ">
+            <Navbar2 />
+            <div className="center">
+              <div className="card-list">
+                {cards}
+              </div>
             </div>
           </div>
-        </div>
-      </div >
+
+          <div className="inline-page ">
+            <Navbar3 />
+            <div className="center">
+              <div className="grid-wrapper">
+                {teamcards}
+              </div>
+            </div>
+          </div>
+
+          <div className="inline-page ">
+            <Navbar4 />
+            <div className="center">
+              <div className="aboutus">
+                <p>We are a small collective based internationally but focusing on building small scale NFT projects. We love to think outside the blockchain and create experiences that holders want to be apart of.</p>
+                <br></br>
+                <p>Exciting things coming soon...</p>
+              </div>
+            </div>
+          </div>
+        </div >
+      </div>
     </div >
   )
 }
---
+++ src/components/Navbarmobile.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar() {
+  return (
+    <navbar className="navbar-mobile">
+      <p>Web3 is just the next step of the internet. We’re building experiences that define that.</p>
+    </navbar>
+  )
+}
